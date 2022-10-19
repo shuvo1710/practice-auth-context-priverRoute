8088ce155da031337d805e35b7bbcab4a6abedcd
shuvo1710 <mdshovo102002@gmail.com>
2022-10-19 18:33:21 +0600

    added card

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const Product = () => {
     return (
         <div>
-            <h2>this is product</h2>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
